1bcc82b22cba25179ea39e1dd0607c3b8c7df3fb
laumonnier <laurent.p.monnier86@gmail.com>
2022-07-04 16:37:22 +0200

    Project initialization: setting up a server, implementing Express, working with the MongoDB application that will allow us to have our database. importing mangoose to link the server to the database.

--- backend/app.js
+++ backend/app.js
@@ -1,24 +1,54 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
 
+mongoose.connect('mongodb+srv://mognal23:LAU12htdp45@cluster0.v4x3lde.mongodb.net/?retryWrites=true&w=majority',
+    {useNewUrlParser: true,
+     useUnifiedTopology: true})
+    .then(() => console.log('Connexion à MongoDB réussi !'))
+    .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+app.use(express.json());
+
 app.use((req, res, next) => {
-    console.log('Requête reçue !');
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Header', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Method', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
     next();
 });
 
-app.use((req, res, next) => {
-    res.status(201);
+app.post('/api/auth/signup',(req, res, next) => {
+
+});
+
+app.post('/api/auth/login',(req, res, next) => {
+
+});
+
+app.get('/api/sauces',(req, res, next) => {
+    res.json('Requête reçue !');
     next();
 });
 
-app.use((req, res, next) => {
-    res.json({message : 'Votre requête a bien été reçue !'});
-    next();
+app.get('/api/sauces/:id', (req, res, next) => {
+
 });
 
-app.use((req, res) => {
-    console.log('Réponse envoyée avec succès !');
+app.post('/api/sauces',(req, res, next) => {
+
+});
+
+app.put('/api/sauces/:id',(req, res, next) => {
+
+});
+
+app.delete('/api/sauces/:id',(req, res, next) => {
+
+});
+
+app.post('/api/sauces/:id/like',(req, res, next) => {
+
 });
 
 module.exports = app;
